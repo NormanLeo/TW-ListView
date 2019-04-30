f0c50c629d3f7e523dcc9b3cba6e5b3d018fdf65
NormanLeo <17010201@myrp.edu.sg>
2019-04-30 15:30:02 +0800

    Commit

--- app/src/main/java/com/myapplicationdev/android/tw_listview/ModuleAdapter.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/ModuleAdapter.java
@@ -2,11 +2,47 @@ package com.myapplicationdev.android.tw_listview;
 
 import android.content.Context;
 
-public class ModuleAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+
+public class ModuleAdapter extends ArrayAdapter<Module> {
+
+    private ArrayList<Module> module;
     private Context context;
+    private TextView tvCode;
+    private ImageView ivProg;
 
+    public ModuleAdapter(Context context, int resource, ArrayList<Module> objects){
+        super(context, resource, objects);
+        module = objects;
+        this.context = context;
+    }
 
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.row, parent, false);
 
+        tvCode = rowView.findViewById(R.id.tvCode);
+        ivProg = rowView.findViewById(R.id.ivProg);
 
+        Module currentFood = module.get(position);
+
+        tvCode.setText(currentFood.getName());
+        if(currentFood.isProg()) {
+            ivProg.setImageResource(R.drawable.prog);
+        }
+        else {
+            ivProg.setImageResource(R.drawable.nonprog);
+        }
+        return rowView;
+    }
 }
--- app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
@@ -3,31 +3,40 @@ package com.myapplicationdev.android.tw_listview;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class SecondActivity extends AppCompatActivity {
 
     ListView lv;
     TextView tvYear;
-
+    ArrayAdapter aa;
+ArrayList<Module> module;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        lv = (ListView) this.findViewById(R.id.lvModules);
-        tvYear = (TextView) findViewById(R.id.tvYear);
+        lv = findViewById(R.id.lvModules);
+        tvYear = findViewById(R.id.tvYear);
+
+        module = new ArrayList<Module>();
+        module.add(new Module("C208", true));
+        module.add(new Module("C200", false));
+        module.add(new Module("C346", true));
+
+        aa = new ModuleAdapter(this, R.layout.row, module);
+        lv.setAdapter(aa);
 
         Intent i = getIntent();
         String year = i.getStringExtra("year");
         tvYear.setText(year);
 
 
-
     }
-
-
 
 }
